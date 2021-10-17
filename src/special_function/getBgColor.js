059b9a247eb20a96c2991376d9fea2f38a196c6f
Andrey Volchenskiy <79094272+andvolch@users.noreply.github.com>
2021-10-17 13:51:13 +0300

    done

--- src/special_function/getBgColor.js
+++
@@ -1,7 +0,0 @@
-const getBgColor = () => {
-  return `rgb(${(Math.random() * 255) << 0}, ${(Math.random() * 255) << 0}, ${
-    (Math.random() * 255) << 0
-  }, 0.5)`;
-};
-
-export default getBgColor;
